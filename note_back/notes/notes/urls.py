e8c39574aa697913e2b25ff39f9c86e1861d1a98
lazaroah <lazaroah02@gmail.com>
2023-07-27 17:21:28 -0400

    fix some bugs

--- note_back/notes/notes/urls.py
+++ note_back/notes/notes/urls.py
@@ -6,7 +6,7 @@ from .schema import schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('graphql/', csrf_exempt(GraphQLView.as_view(schema=schema, graphiql=True)), name = "graphql_api"),
+    path('graphql/', csrf_exempt(GraphQLView.as_view(schema=schema, graphiql=False)), name = "graphql_api"),
 ]
 
 
