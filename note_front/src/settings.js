cccb0c44c8e53246f37e04919c118610b59c5fa0
lazaroah <lazaroah02@gmail.com>
2023-07-27 17:31:41 -0400

    version 1.0 until here

--- note_front/src/settings.js
+++ note_front/src/settings.js
@@ -1,5 +1,4 @@
-//export const BASE_URL = 'http://127.0.0.1:8000/'
+export const BASE_URL = 'http://127.0.0.1:8000/'
 //export const BASE_URL = 'http://192.168.43.2:8000/'
-export const BASE_URL = 'https://greennoteapp.pythonanywhere.com/'
 
 export const GRAPHQL_API = `${BASE_URL}graphql/`
