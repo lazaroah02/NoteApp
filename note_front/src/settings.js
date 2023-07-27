fb2efadf95e110d8491c5ad06b509bff584636bf
lazaroah <lazaroah02@gmail.com>
2023-07-27 19:06:02 -0400

    update the api address

--- note_front/src/settings.js
+++ note_front/src/settings.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://greennoteapp.pythonanywhere.com/"
-//const BASE_URL = "http://127.0.0.1:8000/"
+//const BASE_URL = "https://greennoteapp.pythonanywhere.com/"
+const BASE_URL = "http://127.0.0.1:8000/"
 
 export const GRAPHQL_API = `${BASE_URL}graphql/`
